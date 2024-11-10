0f9502efb98f352bd8c7281545bc81c302e9b19f
bibbb <muhammadhabibalfarabi290803@gmail.com>
2024-11-10 11:04:44 +0700

    Added NGINX configs, added logging with morgan, and a landing HTML page

--- routes/pictureRoute.js
+++ routes/pictureRoute.js
@@ -2,6 +2,13 @@ const router = require('express').Router();
 const multer = require('../libs/multer');
 const PictureController = require('../controllers/pictureController');
 
+router.get('/', (req, res) => {
+    return res.status(200).send(
+        `<h1>Welcome to Art Gallery API!</h1>
+        <a href="/pictures">See all available pictures!</a>
+        <a href="https://github.com/Bibbeep/24001200-km7-mha-file-upload-app-ch6">Github Repository</a>`
+    );
+});
 router.post('/pictures', multer.image.single('image'), PictureController.create);
 router.get('/pictures', PictureController.getAll);
 router.get('/pictures/:id', PictureController.getById);
